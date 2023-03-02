22ca8b879a2e0f22f9d1396a55e957078ba403a6
Hamza <hamzachouchen8@gmail.com>
2023-03-02 16:07:19 +0100

    first commit

---
+++ profile/src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import { useState } from "react";
+import Filter from "./components/Filter";
+import MovieList from "./components/MovieList";
+import { movieData } from "./components/Data";
+import MovieTrailer from "./components/MovieTrailer";
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+
+
+
+function App() {
+  const [movies, setMovies] = useState(movieData);
+  const [filterName, setFilterName] = useState("");
+  const [filterRating, setFilterRating] = useState(0);
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <div className="header">
+          <Filter
+            setFilterName={setFilterName}
+            filterRating={filterRating}
+            setFilterRating={setFilterRating}
+          />
+        </div>
+        
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <MovieList
+                movies={movies}
+                setMovies={setMovies}
+                filterName={filterName}
+                filterRating={filterRating}
+              />
+            }
+          />
+          <Route path="/:title" element={<MovieTrailer movies={movies} />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
